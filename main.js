a9dc15d9276b2357c637a479314b3e3c57df13f9
cristianesavelli <cristiane.cosac@escola.pr.gov.br>
2025-05-30 23:40:22 -0300

    .

--- main.js
+++ main.js
@@ -15,12 +15,11 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-07-09T00:00:00");
-const tempoObjetivo2 = new Date("2025-09-26T00:00:00");
-const tempoObjetivo3 = new Date("2026-08-04T00:00:00");
-const tempoObjetivo4 = new Date("2025-06-15T00:00:00");
+const tempoObjetivo1 = new Date("2025-06-15T00:00:00");
+const tempoObjetivo2 = new Date("2025-11-09T00:00:00");
+const tempoObjetivo3 = new Date("2025-07-04T00:00:00");
 
-const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3];
 
 
 function calculaTempo(tempoObjetivo) {
