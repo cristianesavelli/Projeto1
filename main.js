b641aeb210d1698f421f6031d36b9a54d1aac645
cristianesavelli <cristiane.cosac@escola.pr.gov.br>
2025-05-30 22:52:38 -0300

    .

--- main.js
+++ main.js
@@ -15,12 +15,11 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-06-15T00:00:00");
-const tempoObjetivo2 = new Date("2025-11-09T00:00:00");
-const tempoObjetivo3 = new Date("2025-07-01T00:00:00");
-const tempoObjetivo4 = new Date("2025-08-01T00:00:00");
+const tempoObjetivo1 = new Date("2025-06-15T00:00:00"); //Vest UDESC
+const tempoObjetivo2 = new Date("2025-11-09T00:00:00"); //ENEM
+const tempoObjetivo3 = new Date("2025-07-01T00:00:00"); //FÉRIAS!!!!!!!
 
-const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3];
 
 
 function calculaTempo(tempoObjetivo) {
@@ -41,12 +40,34 @@ function calculaTempo(tempoObjetivo) {
     }
 }
 
-function atualizaCronometro() {
+function atualizaCronometro() { //UDESC
     for (let i = 0; i < contadores.length; i++) {
-        document.getElementById("dias" + i).textContent = calculaTempo(tempos[i])[0];
-        document.getElementById("horas" + i).textContent = calculaTempo(tempos[i])[1];
-        document.getElementById("min" + i).textContent = calculaTempo(tempos[i])[2];
-        document.getElementById("seg" + i).textContent = calculaTempo(tempos[i])[3];
+        document.getElementById("dias0").textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("horas0").textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min0").textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("seg0").textContent = calculaTempo(tempos[i])[3];
+    }
+}
+    }
+}
+
+function atualizaCronometro() { //ENEM
+    for (let i = 0; i < contadores.length; i++) {
+        document.getElementById("dias1").textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("horas1").textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min1").textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("seg1").textContent = calculaTempo(tempos[i])[3];
+    }
+}
+    }
+}
+
+function atualizaCronometro() { //Férias
+    for (let i = 0; i < contadores.length; i++) {
+        document.getElementById("dias2").textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("horas2").textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min2").textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("seg2").textContent = calculaTempo(tempos[i])[3];
     }
 }
 
